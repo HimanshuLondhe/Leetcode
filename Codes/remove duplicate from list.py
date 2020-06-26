b1104183d37d6d9c650fdef59eb2b15cc1b40456
Himanshu Londhe <londhehimanshu@gmail.com>
2020-06-26 14:35:50 -0400

    Update remove duplicate from list.py

--- Codes/remove duplicate from list.py
+++ Codes/remove duplicate from list.py
@@ -5,7 +5,7 @@ mylist = my_function(["a", "b", "a", "c", "c"])
 
 print(mylist)
 
-
+######################################################
 
 
 t = [1, 2, 3, 1, 2, 5, 6, 7, 8]
@@ -13,3 +13,19 @@ t = [1, 2, 3, 1, 2, 5, 6, 7, 8]
 a = list(set(t))
 
 print(a)
+######################################################
+
+def removeDuplicates(self, nums: List[int]) -> int:     #returns length of the duplicate-free array
+        # if len(nums) == 0:
+        #     return 0 
+        # if len(nums) == 1:
+        #     return 1
+        j = 0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        print (nums)
+        return j + 1
+      
+      
